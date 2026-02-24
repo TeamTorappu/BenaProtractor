e5d246baf70b8311951c646ade000b3e4a671eba
Merrow <zeroxilo@outlook.com>
2026-02-25 03:20:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from protractor import PROTRACTOR
 if __name__ == "__main__":
     bena.load_character_names()
     bena.load_enemy_names()
-    #bena.load_buff_template_data()
-    #PROTRACTOR.load_directory("buff_template",bena.BUFF_TEMPLATE_DATA)
+    bena.load_buff_template_data()
+    PROTRACTOR.load_directory("buff_template",bena.BUFF_TEMPLATE_DATA)
     bena.load_roguelike_topic_table()
     PROTRACTOR.load_directory("rogue_item",bena.ROGUELIKE_TOPIC_TABLE)
     PROTRACTOR.open()
