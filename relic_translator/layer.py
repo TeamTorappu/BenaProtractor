0a954db49a55ac3e313e40199e1e4e81cc8bfe92
Merrow <zeroxilo@outlook.com>
2026-06-03 01:15:10 +0800

    生还者合约效果

--- relic_translator/layer.py
+++ relic_translator/layer.py
@@ -9,8 +9,16 @@ def rogue_layer_after_battle_data(item_type,blackboard):
     timing = "战斗结束时，"
     if blackboard.get("is_success",0) != 0:
         timing = "战斗胜利时，"
-    prob = to_percent(blackboard.get("prob",1))
-    scale = blackboard.get("scale")
-    if scale is None:
-        scale = blackboard.get("init","未知")
-    return {"main" : f"{timing}有{prob}概率令本藏品叠加{scale}层（上限{blackboard['max']}层)"}
+    prob = ""
+    if "prob" in blackboard: # 生效概率
+        prob = f"有{to_percent(blackboard['prob'])}概率"
+    scale = blackboard.get("scale",1)
+    features = []
+    if "init" in blackboard: # 初始层数
+        features.append(f"初始{int(blackboard['init'])}层")
+    if "max" in blackboard: # 最大层数
+        features.append(f"最大{int(blackboard['max'])}层")
+        
+    if len(features) > 0:
+        return {"main" : f"{timing}{prob}令本藏品叠加{scale}层（{'，'.join(features)})"}
+    return {"main" : f"{timing}{prob}令本藏品叠加{scale}层"}
